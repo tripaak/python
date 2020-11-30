9d8d97e8b3743f8778c10b41d7f9bd48cd0dd4a6
tripaak <tripaak.akash@gmail.com>
2020-11-30 17:04:58 +0100

    remove some files

--- CodeWithHarry/basic_python/7_read_files.py
+++ CodeWithHarry/basic_python/7_read_files.py
@@ -6,10 +6,14 @@ file = open('akash.txt','r')
 # for line in file:
 #     print(line) # print line by line 
 
+file.tell()   # tell the about the file pointer
 
+file.seek(10) # reset file pointer to charcter 10 
 # print(file.readlines())  # store lines in list
 
 for line in file.readlines():
     print(line)     
 
 file.close()    
+
+
---
+++ CodeWithHarry/basic_python/ex_5_health_management.py
@@ -0,0 +1,24 @@
+# Health management system for 3 client each client has 2 files 1 for diet and 1 for excercise 
+
+# so total 6 files 
+# Client1_diet
+# Client1_excericise
+
+# Client2_diet
+# Client2_excericise
+
+# Client3_diet
+# Client3_excericise
+
+
+# log with timestamp for example
+
+# 22-10-2020 Chicken 
+# 23-10-2020 Rajma chaval
+
+# 1. log diet 
+# 2. log execricse 
+
+# retrieve 
+# 1. diet 
+# 2. excerices 
